376801c4c3b0937d087811e5ba426f674cf003ff
whage <whage@freemail.hu>
2020-06-16 20:10:18 +0200

    Add test cases, implement clock reading logic

--- mirror/mirror.go
+++ mirror/mirror.go
@@ -4,6 +4,26 @@ type ClockReading struct {
 	Hours, Minutes int
 }
 
+func decodeHours(hours int) int {
+	if hours == 11 { return 12}
+	if hours == 12 { return 11}
+	return 11 - hours
+}
+
 func DecodeMirrorImage(in ClockReading) ClockReading {
-	return ClockReading{0, 0}
+	var decodedHours int
+	if in.Minutes == 0 {
+		if in.Hours == 12 {
+			decodedHours = 12
+		} else {
+			decodedHours = 12 - in.Hours
+		}
+	} else {
+		decodedHours = decodeHours(in.Hours)
+	}
+
+	return ClockReading{
+		decodedHours,
+		(60 - in.Minutes) % 60,
+	}
 }
--- mirror/mirror_test.go
+++ mirror/mirror_test.go
@@ -8,6 +8,9 @@ func TestDecodeMirrorImage(t *testing.T) {
 		ClockReading{1, 50},
 		ClockReading{11, 58},
 		ClockReading{12, 1},
+		ClockReading{4, 0},
+		ClockReading{12, 0},
+		ClockReading{6, 0},
 	}
 
 	expectedOutputs := []ClockReading{
@@ -15,12 +18,15 @@ func TestDecodeMirrorImage(t *testing.T) {
 		ClockReading{10, 10},
 		ClockReading{12, 2},
 		ClockReading{11, 59},
+		ClockReading{8, 0},
+		ClockReading{12, 0},
+		ClockReading{6, 0},
 	}
 
 	for idx, expected := range expectedOutputs {
 		result := DecodeMirrorImage(inputs[idx])
 		if result != expected {
-			t.Errorf("Expected `%v`, got %v", expected, result)
+			t.Errorf("Expected %v for %v, got %v instead.", expected, inputs[idx], result)
 		}
 	}
 }
